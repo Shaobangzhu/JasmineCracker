271760c1c3cc532f67ef47eab3bf29b865464d3a
Chaoran Lu <andyluloveusc@gmail.com>
2024-01-05 16:09:11 -0800

    finish calculator expectations

---
+++ simple-calculator/calculator.spec.js
@@ -0,0 +1,32 @@
+describe('calculator.js', function(){
+    it('should add numbers to total', function() {
+        const calculator = new Calculator();
+        calculator.add(5);
+
+        expect(calculator.total).toBe(5);
+    });
+
+    it('should subtract numbers from total', function() {
+        const calculator = new Calculator();
+        calculator.add(5);
+        calculator.subtract(2);
+
+        expect(calculator.total).toBe(3);
+    });
+
+    it('should multiply total by number', function() {
+        const calculator = new Calculator();
+        calculator.add(5);
+        calculator.multiply(3);
+
+        expect(calculator.total).toBe(15);
+    });
+
+    it('should divide total by number', function() {
+        const calculator = new Calculator();
+        calculator.add(25);
+        calculator.divide(5);
+
+        expect(calculator.total).toBe(5);
+    });
+});
