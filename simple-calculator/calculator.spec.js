faf3af6def585aca4173405df7c15acecf6d4f07
Chaoran Lu <andyluloveusc@gmail.com>
2024-01-07 14:42:30 -0800

    Nesting Suites

--- simple-calculator/calculator.spec.js
+++ simple-calculator/calculator.spec.js
@@ -1,34 +1,36 @@
 describe('calculator.js', function(){
 
-    it('should add numbers to total', function() {
-        const calculator = new Calculator();
-        calculator.add(5);
-
-        expect(calculator.total).toBe(5);
-    });
-
-    it('should subtract numbers from total', function() {
-        const calculator = new Calculator();
-        calculator.add(5);
-        calculator.subtract(2);
-
-        expect(calculator.total).toBe(3);
-    });
-
-    it('should multiply total by number', function() {
-        const calculator = new Calculator();
-        calculator.add(5);
-        calculator.multiply(3);
-
-        expect(calculator.total).toBe(15);
-    });
-
-    it('should divide total by number', function() {
-        const calculator = new Calculator();
-        calculator.add(25);
-        calculator.divide(5);
-
-        expect(calculator.total).toBe(5);
+    describe('Calculator', function() {
+        it('should add numbers to total', function() {
+            const calculator = new Calculator();
+            calculator.add(5);
+    
+            expect(calculator.total).toBe(5);
+        });
+    
+        it('should subtract numbers from total', function() {
+            const calculator = new Calculator();
+            calculator.add(5);
+            calculator.subtract(2);
+    
+            expect(calculator.total).toBe(3);
+        });
+    
+        it('should multiply total by number', function() {
+            const calculator = new Calculator();
+            calculator.add(5);
+            calculator.multiply(3);
+    
+            expect(calculator.total).toBe(15);
+        });
+    
+        it('should divide total by number', function() {
+            const calculator = new Calculator();
+            calculator.add(25);
+            calculator.divide(5);
+    
+            expect(calculator.total).toBe(5);
+        });
     });
 
     // toBe "==="
