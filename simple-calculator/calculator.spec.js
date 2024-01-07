ceac4cf8d0d68dfbbbeab3c4d16b176af407401f
Chaoran Lu <andyluloveusc@gmail.com>
2024-01-06 23:43:24 -0800

    toMatch

--- simple-calculator/calculator.spec.js
+++ simple-calculator/calculator.spec.js
@@ -105,4 +105,13 @@ describe('calculator.js', function(){
 
         expect(function() { calculator.divide(0) }).toThrowError(Error, 'Cannot divide by zero');
     });
+
+    // toMatch
+    it('returns total', function() {
+        const calculator = new Calculator();
+        calculator.total = 50;
+
+        expect(calculator.add(20)).toBe(70);
+        expect(calculator.total).toMatch(/-?\d+/);
+    });
 });
